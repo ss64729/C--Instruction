b22eb5e568b02cc4d2940c367290ab3d6e224a78
Sarah Sizemore <sarah.sizemore@kroger.com>
2019-11-01 10:39:57 -0400

    first commit

---
+++ GradeConverter/GradeConverter/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace GradeConverter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Grade Converter");
+            Console.WriteLine();
+
+            
+            Console.WriteLine("Enter a grade");
+            String nbrStr = Console.ReadLine();
+
+            int nbr = Convert.ToInt32(nbrStr);
+
+            if (nbr >= 100)
+            {
+                Console.WriteLine("overachiever");
+            }
+            else
+                if (nbr >= 88)
+            {
+                Console.WriteLine("You have an A!");
+
+            }
+            else
+            if (nbr >= 80)
+            {
+                Console.WriteLine("You have a B");
+            }
+            else 
+            if (nbr >= 67)
+            {
+                Console.WriteLine("You have a C");
+            }
+            else
+            if (nbr >= 60)
+            {
+                Console.WriteLine("You have a D");
+
+            }
+            else Console.WriteLine("You failed.");
+                    }
+               
+           
+
+        
+    }
+}
